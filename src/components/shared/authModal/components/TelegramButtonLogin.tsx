458998a98970604f07864ae883e4f8fa8f1346bc
Abdullo Ergashxo'jayev <abdulloergashxojayev01@gmail.com>
2024-03-10 13:37:37 +0500

    ok

--- src/components/shared/authModal/components/TelegramButtonLogin.tsx
+++ src/components/shared/authModal/components/TelegramButtonLogin.tsx
@@ -1,69 +1,45 @@
-import { CommonButton } from "@/components";
-import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  interface Window {
+    TelegramLoginWidget: {
+      onAuth: (user: any) => void;
+    };
+  }
+}
 
 const TelegramButtonLogin = () => {
+  const ref = useRef<HTMLDivElement>(null);
   const TELEGRAM_BOT_NAME = import.meta.env.VITE_TELEGRAM_BOT_NAME;
 
   useEffect(() => {
-    // Load Telegram Login Widget script dynamically
+    if (ref.current === null) return;
+
+    window.TelegramLoginWidget = {
+      onAuth: (user: any) => {
+        // mutate(user);
+        console.log(user);
+      },
+    };
+
     const script = document.createElement("script");
-    script.src = "https://telegram.org/js/telegram-widget.js?5";
-    script.async = true;
+    script.src = "https://telegram.org/js/telegram-widget.js?4";
     script.setAttribute("data-telegram-login", TELEGRAM_BOT_NAME);
     script.setAttribute("data-size", "large");
     script.setAttribute("data-radius", "10");
-    script.setAttribute("data-auth-url", "/auth"); // Empty since we'll handle the authentication in the script
-    document.body.appendChild(script);
+    script.setAttribute("data-request-access", "write");
+    script.setAttribute("data-userpic", "true");
+    script.setAttribute("data-onauth", "TelegramLoginWidget.onAuth(user)");
+    script.async = true;
+    ref.current.appendChild(script);
 
     return () => {
-      document.body.removeChild(script);
+      ref.current?.removeChild(script);
     };
   }, []);
 
-  const handleTelegramAuth = (user: any) => {
-    // Post user data to your server's authentication endpoint
-    axios
-      .post("/login/telegram", user)
-      .then((response) => {
-        // Handle successful authentication response
-        console.log(response.data);
-      })
-      .catch((error) => {
-        // Handle error
-        console.error("Error:", error);
-      });
-  };
-
-  const triggerTelegramLogin = () => {
-    // Trigger Telegram login button click programmatically
-    const telegramButton = document.querySelector("[data-telegram-login]");
-    if (telegramButton instanceof HTMLElement) {
-      telegramButton.click();
-    }
-  };
-
-  return (
-    <div>
-      <CommonButton
-        className="telegram"
-        title="Telegram orqali kirish"
-        onClick={triggerTelegramLogin}
-      />
-      <script type="text/javascript">
-        {`
-         window.onload = function() {
-          Telegram.Widget.on('auth', function(user) {
-            // Handle user authentication data here
-            console.log(user);
-            // Post user data to your server
-            ${handleTelegramAuth.toString()}(user);
-          });
-        }
-      `}
-      </script>
-    </div>
-  );
+  return <div ref={ref} />;
 };
 
 export default TelegramButtonLogin;
